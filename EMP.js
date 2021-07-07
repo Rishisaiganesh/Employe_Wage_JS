d035a4ee27553a4c0b50db4000c20ec1e6d07fef
Rishisaiganesh <crazyganesh67@gmail.com>
2021-07-07 12:31:17 +0530

    Calculating Employe Wage

--- EMP.js
+++ EMP.js
@@ -1,3 +1,4 @@
+{
 const IS_Absent = 0
 let empCheck = Math.floor(Math.random() * 10)%2;
 if (empCheck == IS_Absent)
@@ -5,6 +6,34 @@ if (empCheck == IS_Absent)
     console.log("Employe is Absent");
 
 }
+
 else{
     console.log("Employe is present");
+}
+
+}
+
+const IS_PART__TIME = 1;
+const IS_FULL_TIME =2;
+const PART_TIME_HOURS = 4;
+const FULL_TIME_HOURS =8;
+const WAGE_PER_HOUR =20;
+{
+    let empHRS =0;
+    let empCheck =Math.floor(Math.random()*10)% 3;
+    switch (empCheck)
+    {
+        case IS_PART__TIME:
+            empHRS = PART_TIME_HOURS;
+            break;
+            
+        case IS_FULL_TIME:
+             empHRS = FULL_TIME_HOURS;
+             break;
+        default:
+            empHRS =0;     
+
+    }
+    let empWage = empHRS * WAGE_PER_HOUR;
+    console.log("Employe Wage is:" + empWage);
 }
