9aaeb3579b4e2ead45f904c2978fe713b92a8660
Rishisaiganesh <crazyganesh67@gmail.com>
2021-07-07 11:26:11 +0530

    EMP_PresentorAbsent

---
+++ EMP.js
@@ -0,0 +1,10 @@
+const IS_Absent = 0
+let empCheck = Math.floor(Math.random() * 10)%2;
+if (empCheck == IS_Absent)
+{
+    console.log("Employe is Absent");
+
+}
+else{
+    console.log("Employe is present");
+}
