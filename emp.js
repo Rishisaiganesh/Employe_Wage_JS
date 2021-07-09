621b8ebe83d291fa5c11322163c3c1363626b936
Rishisaiganesh <crazyganesh67@gmail.com>
2021-07-09 10:40:39 +0530

    Emp Wage per day

--- emp.js
+++ emp.js
@@ -6,3 +6,22 @@ if(check == is_Absent){
 else{
     console.log("Employee is present")
 }
+const emp_is_fulltime=1
+const emp_is_parttime=2
+const emp_full_timehr=8
+const emp_part_timehr=4
+const emp_perhr=20
+    let emphr=0
+    let check=Math.floor(Math.random() * 10) %3
+    switch(check){
+        case emp_is_fulltime:
+            emphr=emp_full_timehr;
+            break;
+            case emp_is_parttime:
+                emphr=emp_part_timehr;
+                break;
+                default:
+                    emphr=0;
+    }
+    let empwage=emphr*emp_perhr;
+    console.log("the empwage is:"+empwage)
