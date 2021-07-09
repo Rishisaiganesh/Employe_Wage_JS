d928b39b15b8362de463dc5c77034fe784821d3e
Rishisaiganesh <crazyganesh67@gmail.com>
2021-07-09 19:19:27 +0530

    calcu empwage using Arrays

--- emp.js
+++ emp.js
@@ -13,49 +13,31 @@ const emp_part_timehr=4
 const emp_perhr=20
 const numof_workingdays = 20
 const maxhrs_inmonth = 160
-{
-    let emphr=0
-    let emp_check=Math.floor(Math.random() * 10) %3
-    switch(emp_check){
-        case emp_is_fulltime:
-            emphr=emp_full_timehr;
-            break;
-            case emp_is_parttime:
-                emphr=emp_part_timehr;
-                break;
-                default:
-                    emphr=0;
-    }
-    let empwage=emphr*emp_perhr;
-    console.log("the empwage is:"+empwage)
-}
-function getEmphr(emp_check)
-{
-    switch(emp_check){
+
+function getEmphr(check){
+    switch(check){
         case emp_is_fulltime:
             return emp_full_timehr;
+            
             case emp_is_parttime:
                 return emp_part_timehr;
+                
                 default:
                     return 0;
     }
 }
-let emp_check=Math.floor(Math.random() * 10) %3
-let emphr=getEmphr(emp_check)
-let empwage=emphr*emp_perhr
-console.log("get emp hour"+emphr+" "+"emp wage"+empwage)
-let totalemphrs=0
-for(let day=0;day<numof_workingdays;day++){
-totalemphrs += getEmphr(emp_check)
-}
-let totalwage = totalemphrs*emp_perhr
-console.log("totalhrs="+totalemphrs + "TOTAL WAGE"+totalwage)
+ function calcuDailyWage(getEmphr){
+     return getEmphr * emp_perhr;
+ }
 
-totalhrs=0
-totaldays=0
+let totalhrs=0;
+let totaldays=0;
+let empDailywageArr = new Array();
 while(totalhrs <= maxhrs_inmonth && totaldays < numof_workingdays){
     totaldays++
-    totalhrs += getEmphr(emp_check)
+    let emp_check = Math.floor(Math.random() *10) % 3;
+    totalhrs += getEmphr(emp_check);
+    empDailywageArr.push(calcuDailyWage(getEmphr));
 }
-let total_wage=totalhrs*emp_perhr
-console.log("hrs="+totalhrs+" "+"days="+totaldays+" "+"wage="+total_wage)
+let total_wage=calcuDailyWage(totalhrs * getEmphr );
+console.log("Total days:" + totaldays+ "Total Hrs" + totalhrs+ "Emp wage:" + total_wage);
